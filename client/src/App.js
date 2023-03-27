eb42427b6605f2890df8aac7e93d62839e175e56
gordondev <usr.kutuzov@gmail.com>
2023-03-27 14:44:24 +0300

    add AppRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Registration from './components/registration';
-import Login from './components/login';
+import React from "react";
+import Registration from "./components/Registration";
+import Login from "./components/Login";
 
 const App = () => {
-    return (
-      <div className="container">
-        <Registration />
-      </div>
-    )
+  return (
+    <div className="container">
+      <Registration />
+    </div>
+  );
 };
 export default App;
