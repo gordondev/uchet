38200bea632d864342d6ab8bb05ab2b347bc5653
gordondev <usr.kutuzov@gmail.com>
2023-03-31 12:11:16 +0300

    fix checklist create

--- client/src/pages/VersionChecklistCreate.jsx
+++ client/src/pages/VersionChecklistCreate.jsx
@@ -2,23 +2,22 @@ import React from "react";
 import { useState } from "react";
 import { createContext } from "react";
 import {
-  Typography,
-  Form,
-  Select,
-  Button,
-  message,
-  Upload,
-  InputNumber,
-  DatePicker,
-  Input,
-  Modal,
+    Typography,
+    Form,
+    Select,
+    Button,
+    message,
+    Upload,
+    InputNumber,
+    DatePicker,
+    Input,
+    Modal,
 } from "antd";
 import {
-  InboxOutlined,
-  PlusOutlined,
-  DeleteOutlined,
-  RollbackOutlined,
-  SaveOutlined,
+    InboxOutlined,
+    PlusOutlined,
+    DeleteOutlined,
+    SaveOutlined,
 } from "@ant-design/icons";
 
 const { Dragger } = Upload;
@@ -30,56 +29,85 @@ const ReachableContext = createContext(null);
 const UnreachableContext = createContext(null);
 
 const props = {
-  name: "file",
-  multiple: true,
-  action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== "uploading") {
-      console.log(info.file, info.fileList);
+  beforeUpload: (file) => {
+    const isDoc = file.type === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
+    if (!isDoc) {
+      message.error(`${file.name} это не docx файл`);
     }
-    if (status === "done") {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === "error") {
-      message.error(`${info.file.name} file upload failed.`);
-    }
+    return isDoc || Upload.LIST_IGNORE;
   },
-  onDrop(e) {
-    console.log("Dropped files", e.dataTransfer.files);
-  },
+    multiple: false,
+    action: "http://localhost:3000/versionchecklist/create",
+    onChange(info) {
+        const { status } = info.file;
+        if (status !== "uploading") {
+            console.log(info.file, info.fileList);
+        }
+        if (status === "done") {
+            message.success(`${info.file.name} файл успешно загружен`);
+        } else if (status === "error") {
+            message.error(`${info.file.name} ошибка загрузки файла`);
+        }
+    },
+    onDrop(e) {
+        console.log("Dropped files", e.dataTransfer.files);
+    },
 };
 
 const config = {
-  title: "Предупреждение!",
-  content: (
-    <>
-      <ReachableContext.Consumer>
+    title: "Предупреждение!",
+    content: ( <
+        >
+        <ReachableContext.Consumer>
         {(name) => `С текущей записи будет снят данный ключ`}
-      </ReachableContext.Consumer>
-    </>
-  ),
+      </ReachableContext.Consumer> <
+        />
+    ),
 };
-
+let countTheme = 0;
 const VersionChecklistCreate = () => {
-  const [modal, contextHolder] = Modal.useModal();
-  return (
-    <ReachableContext.Provider value="Light">
+    const [modal, contextHolder] = Modal.useModal();
+    const [info, setInfo] = useState([]);
+    const [count, setCount] = useState(1);
+
+    const addInfo = () => {
+        setCount(count + 1);
+        setInfo([...info, { theme: "", number: Date.now() }]);
+    };
+
+    const removeInfo = (number) => {
+        setCount(count - 1);
+        setInfo(info.filter((i) => i.number !== number));
+    };
+
+    const changeInfo = (value, number) => {
+        setInfo(
+            info.map((i) => (i.number === number ? { ...i, theme: value } : i))
+        );
+    };
+
+    return (
+        <ReachableContext.Provider value="Light">
       <section className="searchSection">
         <div className="container">
+        <Form>
           <div className="defaultForm">
             <div className="defaultForm__tile">
-              <Title
-                className="blockWelcome__title"
-                level={3}
-                style={{ color: "#0e78ff", marginBottom: "20px" }}
-              >
-                Версия №135002
-              </Title>
+              <Form.Item label="Номер версии" style={{ marginBottom: "20px" }}>
+                <InputNumber min={1} defaultValue={1} prefix="№" 
+                  rules={[
+                  {
+                    required: true,
+                    message: "Выберите номер версии",
+                  },
+                ]}
+                />
+              </Form.Item>
               <Form.Item
                 name="select"
                 label="Ключ актуальности"
                 hasFeedback
-                style={{ width: "300px" }}
+                style={{ width: "400px" }}
                 rules={[
                   {
                     required: true,
@@ -103,31 +131,47 @@ const VersionChecklistCreate = () => {
               level={4}
               style={{ marginBottom: "20px", textAlign: "center" }}
             >
-              Темы:
+              Темы
             </Title>
-            <div className="theme_item">
-              <p>1.</p>
-              <Input placeholder="Название темы" allowClear />
-              <Button
-                type="primary"
-                danger
-                htmlType="submit"
-                style={{ marginLeft: "20px" }}
-                icon={<DeleteOutlined />}
-              >
-                Удалить
-              </Button>
-            </div>
+            {info.map((i) => (
+              <div className="theme_item">
+                <Form.Item
+                  name={i.number}
+                  label="Название темы"
+                  style={{ marginTop: "23px", width: "100%"}}
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Введите название темы',
+                    },
+                  ]}
+                >
+                <Input />
+                </Form.Item>
+                  <Button
+                    type="primary"
+                    danger
+                    htmlType="submit"
+                    style={{ marginLeft: "20px" }}
+                    icon={<DeleteOutlined />}
+                    onClick={() => removeInfo(i.number)}
+                  >
+                    Удалить
+                  </Button>
+              </div>
+            ))}
+
             <Button
               type="primary"
-              htmlType="submit"
               style={{ width: "100%", marginBottom: "20px" }}
               icon={<PlusOutlined />}
+              onClick={addInfo}
+              disabled={count >= 7}
             >
-              Добавить
+              Добавить, осталось: {6 - (count - 1)}
             </Button>
             <div className="defaultForm__dragBlock">
-              <Dragger {...props} style={{ padding: "20px" }}>
+              <Dragger {...props} maxCount={1} style={{ marginBottom: "20px" }}>
                 <p className="ant-upload-drag-icon">
                   <InboxOutlined />
                 </p>
@@ -138,7 +182,7 @@ const VersionChecklistCreate = () => {
                   Прикрепите файл шапки чек-листа
                 </p>
               </Dragger>
-              <Dragger {...props} style={{ padding: "20px" }}>
+              <Dragger {...props} maxCount={1}>
                 <p className="ant-upload-drag-icon">
                   <InboxOutlined />
                 </p>
@@ -161,7 +205,12 @@ const VersionChecklistCreate = () => {
                 style={{ width: "100%" }}
               />
             </Form.Item>
-            <Form.Item label="Дата принятия">
+            <Form.Item label="Дата принятия" name="date-picker" rules={[
+                    {
+                      required: true,
+                      message: 'Выберите дату принятия',
+                    },
+                  ]}>
               <DatePicker style={{ width: "100%" }} />
             </Form.Item>
             <Form.Item label="Основание использования">
@@ -170,20 +219,8 @@ const VersionChecklistCreate = () => {
             <Form.Item label="Примечание">
               <TextArea rows={4} />
             </Form.Item>
-            <div className="defaultForm__buttonsBottom">
-              <Button
-                type="primary"
-                htmlType="submit"
-                icon={<RollbackOutlined />}
-                style={{
-                  width: "100%",
-                  marginBottom: "20px",
-                  marginRight: "20px",
-                }}
-              >
-                Назад
-              </Button>
-              <Button
+              <Form.Item style={{ width: "100%" }}>
+                <Button
                 type="primary"
                 htmlType="submit"
                 icon={<SaveOutlined />}
@@ -191,14 +228,16 @@ const VersionChecklistCreate = () => {
               >
                 Сохранить
               </Button>
-            </div>
+              </Form.Item>
           </div>
+        </Form>
+          
         </div>
       </section>
       {contextHolder}
       <UnreachableContext.Provider value="Bamboo" />
     </ReachableContext.Provider>
-  );
+    );
 };
 
 export default VersionChecklistCreate;
