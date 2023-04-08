35c0d7b22d80cfff44df36b2fcd87309f36eacaa
gordondev <usr.kutuzov@gmail.com>
2023-04-08 18:30:31 +0300

    add search in versions

--- client/src/pages/VersionChecklist.jsx
+++ client/src/pages/VersionChecklist.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Input, Col, Row, FloatButton, Card} from "antd";
+import React, { useEffect, useState, useMemo } from "react";
+import { Input, Col, Row, FloatButton, Card, Empty } from "antd";
 import { PlusOutlined, EditOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 import { observer } from "mobx-react-lite";
@@ -14,34 +14,51 @@ const VersionChecklist = observer(() => {
   const [data, setData] = useState([]);
   const [fetching, setFetching] = useState(true);
   const [totalCount, setTotalCount] = useState(0);
+
+  // const [selectedSort, setSelectedSort] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+
+  // const sortedVersions = useMemo( () => {
+  //   if (selectedSort) {
+  //     return [...data].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+  //   }
+  // }, [selectedSort, data] );
+
   const navigate = useNavigate();
 
-  useEffect( () => {
+  useEffect(() => {
     if (fetching) {
-      console.log('fetching');
-      fetchVersionChecklist(24, currentPage).then(response => { 
-        setData([...data, ...response.rows]);
-        console.log("USE EFFECT", data.length, totalCount);
-        setCurrentPage(prevState => prevState + 1);
-        setTotalCount(response.count);
-      }).finally(() => setFetching(false));
+      fetchVersionChecklist(24, currentPage)
+        .then((response) => {
+          setData([...data, ...response.rows]);
+          setCurrentPage((prevState) => prevState + 1);
+          setTotalCount(response.count);
+        })
+        .finally(() => setFetching(false));
     }
   }, [fetching]);
 
-  useEffect( () => {
-    document.addEventListener('scroll', scrollHendler)
+  useEffect(() => {
+    document.addEventListener("scroll", scrollHendler);
     return () => {
-      document.removeEventListener('scroll', scrollHendler)
+      document.removeEventListener("scroll", scrollHendler);
     };
-
   }, [totalCount]);
 
   const scrollHendler = (e) => {
-    console.log("EVENT SCROLL", data.length, totalCount);
-    if (e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100 && data.length <totalCount) {
+    if (
+      e.target.documentElement.scrollHeight -
+        (e.target.documentElement.scrollTop + window.innerHeight) <
+        100 &&
+      data.length < totalCount
+    ) {
       setFetching(true);
     }
-  }
+  };
+
+  const sortedAndSearchedVersions = useMemo(() => {
+    return data.filter((data) => String(data.id).includes(searchQuery));
+  }, [searchQuery, data]);
 
   return (
     <section className="searchSection">
@@ -50,25 +67,46 @@ const VersionChecklist = observer(() => {
           placeholder="Введите версию"
           allowClear
           enterButton="Поиск"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
           size="large"
           style={{ width: "100%" }}
         />
-
-        <Row gutter={[40, 16]}>
-          {data.map(data =>
-            <Col className="gutter-row" span={6} key={data.id}>
-              <Card
+          <Row gutter={[40, 16]} justify="left">
+          {sortedAndSearchedVersions.length ? sortedAndSearchedVersions.map((data) => (
+            <Col className="gutter-row" span={sortedAndSearchedVersions.length == 2 ? 12 : 8}
+              key={data.id}>
+              {
+                data.actualKey == "Не актуально" ?
+                  <Card
+                  hoverable
+                  style={{
+                    minWidth: 270,
+                    marginTop: 16
+                  }}
+                  actions={[<EditOutlined key="edit" />]}
+                >
+                  <Meta title={"Версия №" + data.id} description={data.actualKey} />
+                </Card>
+                :
+                <Card
+                hoverable
                 style={{
                   minWidth: 270,
                   marginTop: 16,
+                  backgroundColor: "#52c41a"
                 }}
                 actions={[<EditOutlined key="edit" />]}
               >
-                <Meta title={"Версия №" + data.id} description="Не актуально" />
+                <Meta title={"Версия №" + data.id} description={data.actualKey} />
               </Card>
+              }
+              
             </Col>
-          )}
+          )) : <Empty style={{ marginTop: "20px" }}/>}
         </Row>
+        
+        
         <FloatButton
           icon={<PlusOutlined />}
           type="primary"
