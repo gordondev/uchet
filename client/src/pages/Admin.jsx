e4d0fcdb548369b89d66eda96e5117a26afc4759
gordondev <usr.kutuzov@gmail.com>
2023-05-14 15:58:25 +0300

    fix admin form

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,20 +1,49 @@
 import React, { useState, useEffect } from "react";
-import { Table, Button } from "antd";
+import { Table, Button, Modal, Form, Input, Select, message } from "antd";
 import { getAllUsers } from "../http/userAPI";
 import { observer } from "mobx-react-lite";
 import { DeleteOutlined, LockOutlined, EditOutlined } from "@ant-design/icons";
+import { debounce } from 'lodash';
+
+const { Option } = Select;
 
 const Admin = observer(() => {
+  const [form] = Form.useForm();
   const [isLoadind, setIsLoading] = useState(true);
   const [users, setUsers] = useState([]);
-
-  console.log(users);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingUser, setEditingUser] = useState(null);
+  const [division, setDivision] = useState("");
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [patronymic, setPatronymic] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
 
   useEffect(() => {
     setIsLoading(true);
     getAllUsers().then((data) => setUsers(data));
     setIsLoading(false);
   }, []);
+
+  const onEditUser = (record) => {
+    setIsEditing(true);
+    setEditingUser({...record});
+  }
+
+  const resetEditing = () => {
+    setIsEditing(false);
+    setEditingUser(null);
+    setDivision("");
+    setName("");
+    setSurname("");
+    setPatronymic("");
+    setEmail("");
+    setPassword("");
+    setRole("");
+    form.resetFields();
+  }
 
   const columns = [
     {
@@ -150,8 +179,8 @@ const Admin = observer(() => {
       dataIndex: "",
       key: "x",
       width: 165,
-      render: () => (
-        <Button type="primary" icon={<EditOutlined />}>
+      render: (record) => (
+        <Button type="primary" icon={<EditOutlined />} onClick={() => { onEditUser(record) }}>
           Редактировать
         </Button>
       ),
@@ -184,6 +213,122 @@ const Admin = observer(() => {
           }}
           loading={isLoadind}
         />
+        <Modal
+          centered
+          title="Редактирование пользователя"
+          okText="Сохранить"
+          cancelText="Отмена"
+          visible={isEditing}
+          onCancel={() => resetEditing()}
+          onOk={() => resetEditing()}
+        >
+          <Form
+            className="formWindow"
+            name="register"
+            form={form}
+            style={{
+              maxWidth: 470,
+            }}
+            scrollToFirstError
+          >
+            <Form.Item
+              name="division"
+              label="Подразделение"
+              hasFeedback
+              onChange={(e) => setDivision(e.target.value)}
+            >
+              <Select
+                defaultValue={division === "" && null}
+                placeholder={editingUser?.division}
+                allowClear
+              >
+                <Option value="ТЦ-3">ТЦ-3</Option>
+                <Option value="РЦ-2">РЦ-2</Option>
+                <Option value="РЦ-3">РЦ-3</Option>
+                <Option value="ЦЦР">ЦЦР</Option>
+                <Option value="ЦОРО">ЦОРО</Option>
+                <Option value="ЭЦ">ЭЦ</Option>
+                <Option value="ЦТАИ">ЦТАИ</Option>
+                <Option value="ЦВ">ЦВ</Option>
+                <Option value="ОРБ">ОРБ</Option>
+                <Option value="ХЦ">ХЦ</Option>
+                <Option value="ТЦ-2">ТЦ-2</Option>
+                <Option value="РТЦ-1">РТЦ-1</Option>
+                <Option value="ЦОС">ЦОС</Option>
+                <Option value="ОПБ">ОПБ</Option>
+                <Option value="ОЯБиН">ОЯБиН</Option>
+                <Option value="Управление">Управление</Option>
+                <Option value="ОТИиПБ">ОТИиПБ</Option>
+                <Option value="ОИиКОБ">ОИиКОБ</Option>
+                <Option value="ООТ">ООТ</Option>
+                <Option value="УТП">УТП</Option>
+              </Select>
+            </Form.Item>
+
+            <Form.Item
+              name="role"
+              label="Роль"
+              hasFeedback
+              onChange={(e) => setRole(e.target.value)}
+            >
+              <Select
+                defaultValue={role === "" && null}
+                placeholder={editingUser?.role}
+                allowClear
+              >
+                <Option value="USER">USER</Option>
+                <Option value="ADMIN">ADMIN</Option>
+              </Select>
+            </Form.Item>
+
+            <Form.Item
+              name="name"
+              label="Имя"
+              onChange={debounce((e) => setName(e.target.value), 500)}
+            >
+              <Input placeholder={editingUser?.name} allowClear />
+            </Form.Item>
+
+            <Form.Item
+              name="surname"
+              label="Фамилия"
+              onChange={debounce((e) => setSurname(e.target.value), 500)}
+            >
+              <Input placeholder={editingUser?.surname} allowClear />
+            </Form.Item>
+
+            <Form.Item
+              name="patronymic"
+              label="Отчество"
+              onChange={debounce((e) => setPatronymic(e.target.value), 500)}
+            >
+              <Input placeholder={editingUser?.patronymic} allowClear />
+            </Form.Item>
+
+            <Form.Item
+              name="email"
+              label="Почта"
+              onChange={debounce((e) => setEmail(e.target.value), 500)}
+              rules={[
+                {
+                  type: "email",
+                  message: "Не валидная почта",
+                },
+              ]}
+            >
+              <Input placeholder={editingUser?.email} allowClear />
+            </Form.Item>
+
+            <Form.Item
+              name="password"
+              label="Пароль"
+              onChange={debounce((e) => setPassword(e.target.value), 500)}
+              hasFeedback
+            >
+              <Input.Password allowClear />
+            </Form.Item>
+          </Form>
+        </Modal>
       </div>
     </section>
   );
